b55ad9b01ad3b123f5ed8cbbfcc056d963f10350
Yone Shinzato <yshinzato@fundasp.org.br>
2020-06-11 15:45:04 -0300

    1

---
+++ js/desafio-1-4.js
@@ -0,0 +1,13 @@
+// Crie um programa para realizar operações bancárias na conta de um usuário.
+// Comece criando um objeto com o nome do usuário, suas transações e saldo.
+// As transações (transactions) devem iniciar como um array vazio [] e o saldo (balance) em 0 (zero).
+
+const user = {
+    name: "Mariana",
+    transactions: [],
+    balance: 0
+  };
+
+  function createTransaction() {
+      user.transactions.push()
+  }
